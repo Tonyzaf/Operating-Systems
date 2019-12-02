6cc553719f02b56bafeb7d11508062113dcb0da8
Antonis Zafeirelis <zaftony@hotmail.gr>
2019-12-02 12:30:45 +0200

    Add files via upload
    final frickin version

--- OS Project #1/exc1_4.c
+++ OS Project #1/exc1_4.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <time.h>
+#include <sys/wait.h>
 void foo(){
     int x = 0;
     x = x + 10;
@@ -11,13 +12,19 @@ int main(){
     printf("The time is %ld\n",start);
     int pid;
     int i = 0;
-    while(i<100){
+    while(i<1000){
         pid = fork();
         if(pid == 0){
             foo();
-            printf("%d\n",i);
             break;
         }
     i++;
     }
+    for(int y = 0;y<10000;y++){
+        waitpid(0,NULL,WUNTRACED);
+    }
+    time_t end = time(NULL);
+    double z = (double)end - (double)start;
+    z = z / 1000;
+    printf("Time to end: %f\n",z);
 }
